1b13b5d0c0ee6e694211fa04ce7c695b89e871a3
BOOGIEDML <mac@Boogiedmls-MBP.lan>
2023-08-30 20:14:14 +0100

    Add Get In touch

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { GiInfinity } from "react-icons/gi";
 
 const Navbar = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const menu = [
     { name: "home", link: "/" },
     { name: "About", link: "/about" },
@@ -25,8 +26,7 @@ const Navbar = () => {
             <li
               key={m.name + i}
               className={`uppercase text-[12px] relative cursor-pointer tracking-[1px] text-gray-100 font-nunito font-[500] ${
-                m.link === location.pathname &&
-                "active text-white"
+                m.link === location.pathname && "active text-white"
               }`}
             >
               {m.name}
@@ -37,7 +37,10 @@ const Navbar = () => {
       <div className="w-12 sm:w-14 h-3.5 relative block lg:hidden cursor-pointer">
         <span className="before:absolute before:top-0 before:bg-white before:w-full before:h-[2px] before:rounded-lg after:absolute after:bottom-0 after:bg-white after:w-[80%] after:right-0 after:h-[2px] after:rounded-lg"></span>
       </div>
-      <button className="bg-white hidden lg:block text-base font-grotesk font-[500] py-2 px-4 rounded hover:bg-gray-200 transition-all duration-300">
+      <button
+        onClick={() => navigate("/get-in-touch")}
+        className="bg-white hidden lg:block text-base font-grotesk font-[500] py-2 px-4 rounded hover:bg-gray-200 transition-all duration-300"
+      >
         Get in touch
       </button>
     </nav>
