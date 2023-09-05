602beb94cd4f95d7d3cd77fa908b08fcd75720a0
BOOGIEDML <mac@Boogiedmls-MacBook-Pro.local>
2023-09-05 02:18:11 +0100

    redesign nav-menu

--- src/containers/MiniNavbar.jsx
+++ src/containers/MiniNavbar.jsx
@@ -13,7 +13,7 @@ const MiniNavbar = () => {
       <nav className="flex items-center justify-between py-12 px-5 md:px-9 lg:px-12 border-b-[1px] border-black">
         <span
           onClick={() => navigate("/")}
-          className="logo group flex items-center font-grotesk text-lg text-black hover:text-gray-700 transition-all duration-300 cursor-pointer"
+          className="logo group flex items-center relative z-[150] font-grotesk text-lg text-black hover:text-gray-700 transition-all duration-300 cursor-pointer"
         >
           Codel
           <GiInfinity className="text-3xl animate-spin antialiased group-hover:animate-none" />
